c324913f533e25c1fac35b25aa3d081600774a97
Luke Chinworth & Dale Palmer <student@epicodus.com>
2015-01-13 11:12:28 -0800

    add remaining logic and specs.

--- lib/places_been.rb
+++ lib/places_been.rb
@@ -1,9 +1,25 @@
 class Place
+
+  @@all_locations = []
+
   define_method(:initialize) do |location|
     @location = location
   end
 
+  define_singleton_method(:bucket) do
+    @@all_locations
+  end
+
   define_method(:location) do
     @location
+  end
+
+  define_method(:save) do
+    @@all_locations.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_locations = []
+
   end
 end
--- spec/places_been_spec.rb
+++ spec/places_been_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('./lib/places_been')
 
 describe(Place) do
+  before() do
+    Place.clear()
+  end
+  
   describe("#location") do
     it("lets you name a location") do
       test_task = Place.new("Portland, OR")
@@ -9,10 +13,27 @@ describe(Place) do
     end
   end
 
-  # describe("#save") do
-  #   it("Saves a string that we input") do
-  #     test_place = Place.new()
-  #     expect(test_place.save()).to(eq())
-  #   end
-  # end
+  describe(".bucket") do
+    it("It gives a bucket to put stuff in") do
+      expect(Place.bucket()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("Saves a string that we input") do
+      test_place = Place.new("Portland, OR")
+      test_place_2 = Place.new("Tulsa, OK")
+      test_place.save()
+      test_place_2.save()
+      expect(Place.bucket()).to(eq([test_place, test_place_2]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved locations") do
+      Place.new("Crater Lake").save()
+      Place.clear()
+      expect(Place.bucket()).to(eq([]))
+    end
+  end
 end
