c9248d4b291e468c7d7315fceba8563b96479e7e
Luke Chinworth & Dale Palmer <student@epicodus.com>
2015-01-13 11:42:57 -0800

    add sinatra app integration.

---
+++ app.rb
@@ -0,0 +1,15 @@
+require("sinatra")
+require("sinatra/reloader")
+require("./lib/places_been")
+
+get('/') do
+  @locations = Place.bucket()
+  erb(:location_list)
+end
+
+post('/confirmation') do
+  location_string = params.fetch("location")
+  location = Place.new(location_string)
+  location.save()
+  erb(:confirmation)
+end
